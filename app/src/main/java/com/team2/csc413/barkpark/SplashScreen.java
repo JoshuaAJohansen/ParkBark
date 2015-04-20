9026b182e90e8caed30b3cbf301d2fa3a826a233
Josh <joshjohansen42@gmail.com>
2015-04-20 10:52:14 -0700

    init commit

---
+++ app/src/main/java/com/team2/csc413/barkpark/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.team2.csc413.barkpark;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Context c = this;
+        Thread time = new Thread(){
+            public void run(){
+                try{
+
+                    sleep(2000);
+
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent openStartingPoint = new Intent(c,MainActivity.class);
+                    startActivity(openStartingPoint);
+                }
+            }};
+        time.start();
+    }
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+}
