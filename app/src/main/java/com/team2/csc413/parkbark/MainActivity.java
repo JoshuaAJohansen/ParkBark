960e678d2a2be2a0773ef13fb7918cd1f3307efe
mykevin81 <mykevin81@gmail.com>
2015-04-21 04:12:16 -0700

    Added marker function on the map *The Parking button is located on the bottom right. *Pressing Parking button again will remove the parking marker

--- app/src/main/java/com/team2/csc413/parkbark/MainActivity.java
+++ app/src/main/java/com/team2/csc413/parkbark/MainActivity.java
@@ -16,13 +16,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 //TODO Include Park button that store park location and mark it on the map
 
@@ -31,6 +36,8 @@ import com.google.android.gms.maps.model.LatLng;
 public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
     GoogleMap mMap;
+    Marker ParkMarker = null;
+
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -47,6 +54,8 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ImageButton Park_Button = (ImageButton) findViewById(R.id.Park_Btn);
+
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
@@ -59,6 +68,41 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        //Park Button On-Click listener
+        Park_Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+
+                LocationManager locationmanager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationmanager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(location == null){
+                    Toast.makeText(getApplicationContext(), "Cannot find Location: location == NULL", Toast.LENGTH_SHORT).show();
+                }else if(ParkMarker == null) {
+
+                    LatLng PARKED = new LatLng (location.getLatitude(), location.getLongitude());
+                    ParkMarker = mMap.addMarker(new MarkerOptions()
+                                                    .position(PARKED)
+                                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marker)));
+
+                    String Text = "Parking Location: \n" +
+                            "Latitude: " + location.getLatitude() + "\n" +
+                            "Longitude: " + location.getLongitude();
+
+                    Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_SHORT).show();
+
+
+                }else if(ParkMarker != null){
+
+                    mMap.clear();
+
+                    Toast.makeText(getApplicationContext(), "Leaving Parking Spot", Toast.LENGTH_SHORT).show();
+
+                    ParkMarker = null;
+                }
+            }
+        });
 
     }
 
