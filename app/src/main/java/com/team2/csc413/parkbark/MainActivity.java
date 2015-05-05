d43b26b49483300e559df602e33330340054c0ef
Michael <openmichael65@gmail.com>
2015-05-05 01:00:10 -0700

    Add onMapLongClick method

--- app/src/main/java/com/team2/csc413/parkbark/MainActivity.java
+++ app/src/main/java/com/team2/csc413/parkbark/MainActivity.java
@@ -225,8 +225,6 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerF
                     }
                 });
                 builder.show();
-
-
             }
         });
 
