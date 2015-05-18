12a3f15be8e86857a62824487f54f8f9761c520b
Majiccow <joshjohansen42@gmail.com>
2015-05-18 14:59:24 -0700

    Deleted extra unused package imports

--- app/src/main/java/com/team2/csc413/parkbark/MainActivity.java
+++ app/src/main/java/com/team2/csc413/parkbark/MainActivity.java
@@ -26,8 +26,6 @@ import android.os.CountDownTimer;
 import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
